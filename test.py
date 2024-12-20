a9e2d7f19dffcdd2e4b1f07018ebf0c8e6f5b53b
Yael Chen <51058584+YaelChen@users.noreply.github.com>
2024-12-20 10:28:50 +0200

    Update test.py
    canceled the option to use the same number twice.

--- test.py
+++ test.py
@@ -1,48 +1,53 @@
-"""1. give me python method that gets a sorted list of numbers
+"""1. give me Python method that gets a sorted list of numbers
 and returns 2 numbers that equal to input"""
 
 
-def nums_from_list(sorted_list, target_num):
-    """a function that gets a sorted list of numbers and an int,
-    and returns two nums from the list that their sum is equal to the int input.
+
+def nums_from_list(numlist, target_num):
+    """a function that gets a list of numbers and an int,
+    and returns two nums from the list whose sum is equal to the int input.
     :param: a list of ints and a number.
     :type: list, int
     :return: two numbers from the list
     :rtype: tup"""
 
-    for first_num in sorted_list:
+    for first_num in numlist:
         second_num = target_num - first_num
-        if second_num in sorted_list:
+        templist = numlist.copy()
+        templist.remove(first_num)
+        if second_num in templist:
             return first_num, second_num
     return -1, -1
 
 
-sortedlist = [1, 2, 3, 4, 5, 6, 7]
-target = 7
+unsortedlist = [32, 24, 4, 7, 12, 9, 18, 2, 8, 40, 6, 2, 3, 1]
+target = 80
 
-print(nums_from_list(sortedlist, target))
+print(nums_from_list(unsortedlist, target))
 
 
 """2. Same as 1, but return a list with the 2 numbers"""
 
 
-def numlist_from_list(sorted_list, target_num):
+def numlist_from_list(numlist, target_num):
     """a function that gets a sorted list of numbers and an int,
-    and returns a list of two nums from the list that their sum is equal to the int input.
+    and returns a list of two nums from the list whose sum is equal to the int input.
     :param: a list of ints and a number.
     :type: list, int
     :return: a list of two numbers
     :rtype: list"""
 
-    for first_num in sorted_list:
+    for first_num in numlist:
         second_num = target_num - first_num
-        if second_num in sorted_list:
+        templist = numlist.copy()
+        templist.remove(first_num)
+        if second_num in templist:
             return [first_num, second_num]
     return [-1, -1]
 
 
-sortedlist = [1, 2, 3, 4, 5, 6, 7]
+unsortedlist = [32, 24, 4, 7, 12, 9, 18, 2, 8, 40, 6, 2, 3, 1]
 target = 13
 
-print(numlist_from_list(sortedlist, target))
+print(numlist_from_list(unsortedlist, target))
 
